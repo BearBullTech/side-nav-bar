fb07f57d83d244c87ac969c116be662dfc5a952c
Roy Lee <roylee@Andrews-MacBook-Pro.local>
2018-09-10 14:42:48 -0700

    added eslint

--- db/db.js
+++ db/db.js
@@ -1,25 +1,25 @@
 const mongoose = require('mongoose');
-const URI = 'mongodb://localhost/fecdata';
-var db = mongoose.connection;
+// const URI = 'mongodb://localhost/fecdata';
+// var db = mongoose.connection;
 
 //===== DBSchema =======
-// var Schema = mongoose.Schema;
-// const companiesSchema = new mongoose.Schema({
-// 	company: String,
-// 	currentDay: Array
-// })
 
-
-
-//======================
-
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-  // we're connected!
-  console.log("we're connected to the DB!");
+const companiesSchema = new mongoose.Schema({
+	company: String,
+	currentDay: Array
 });
 
+const Company = mongoose.model('Company', companiesSchema);
+
+
+// var db = mongoose.connection;
+// db.on('error', console.error.bind(console, 'connection error:'));
+// db.once('open', function() {
+//   // we're connected!
+//   console.log("we're connected to the DB!");
+// });
+
+module.exports = Company;
 //save multiple data files look up 'bulkwrite'
 
 // var userSchema = new mongoose.Schema({
